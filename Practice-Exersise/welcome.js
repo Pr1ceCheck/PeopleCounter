879efe2b0528a02b1b7d2e21e9fc99de6c19b9cf
PR1CETAG <cprice@atl.yearup.og>
2022-10-15 10:28:15 -0400

    not debugged peoplecounter

---
+++ Practice-Exersise/welcome.js
@@ -0,0 +1,16 @@
+//Grab the welcome-al paragraph and store it in a variable 
+//create to variables (name & greeting) that contain name
+// render greeting to page
+// render welcome message using welcomeAl.innertext
+
+let welcomeAl = document.getElementById("welcome-al")
+let userName = Trez
+let greetMess = "Have a nice day "
+welcomeAl.innerText = greetMess + userName
+
+//add an emoji to the end
+//write code
+//hint****count = count + 1
+
+//welcomeAl.innerText = welcomeAl.innerText + //"emoji" (Line 16 the short way)
+//welcomeAl += emoji
--- index.js
+++ index.js
@@ -4,13 +4,19 @@
 // change the count-ed in the HTML to reflect the new count 
 //------------ steps -----------
 let countEd = document.getElementById("count-ed") 
-
+let paraEd = document.getElementById("para-ed")
 let count = 0
-
-
+let showSaved = count + " - "
 //declare funtion
 function increment () {
 count = count + 1
-countEd.innerText = count
+countEd.innerText = count//can change like ln 13
 }
 //document.getElementById(count).innerText = count
+let results = 0 
+
+function save() {
+paraEd.textContent += showSaved // did not use inner.text
+console.log(count)
+}
+
